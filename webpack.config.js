7a8aa4349729865e0b3a26899b4a439ae3a82904
anderson2811 <anderson.andres.guerrero@gmail.com>
2019-09-01 15:30:30 -0500

    PWA

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const WebpackPwaManifestPlugin = require('webpack-pwa-manifest')
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin')
 const path = require('path')
 
 module.exports = {
@@ -11,6 +12,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: 'src/index.html'
     }),
+    // generate Manifeste file. PWA
     new WebpackPwaManifestPlugin({
       name: 'PetGram - Tu app de fotos de mascotas',
       shortname: 'PetGram - :)',
@@ -20,7 +22,30 @@ module.exports = {
       icons: [
         {
           src: path.resolve('src/assets/icon.png'),
-          sizes: [96, 128, 192, 256, 384, 5121]
+          sizes: [96, 128, 192, 256, 384, 5121],
+          ios: true
+        }
+      ]
+    }),
+    // Configuracion offline app
+    new WorkboxWebpackPlugin.GenerateSW({
+      runtimeCaching: [
+        {
+          urlPattern: new RegExp('https://(res.cloudinary.com|images.unsplash.com)'),
+          // Revisa el cache primero
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'images'
+          }
+        },
+        {
+          // cache offline para el api
+          urlPattern: new RegExp('https://petgram-server.now.sh'),
+          // Revisa la network primero
+          handler: 'NetworkFirst',
+          options: {
+            cacheName: 'api'
+          }
         }
       ]
     })
